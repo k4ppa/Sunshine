3bc645ee4c240367d6939176e62892d4d9a6775e
k4ppa <cappa.mattia@gmail.com>
2015-06-09 17:35:04 +0200

    Refresh using alarm

--- app/src/main/java/com/example/sunshine/app/service/SunshineService.java
+++ app/src/main/java/com/example/sunshine/app/service/SunshineService.java
@@ -307,4 +307,12 @@ public class SunshineService extends IntentService {
         return locationId;
     }
 
+    static public class AlarmReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent sendIntent = new Intent(context, SunshineService.class);
+            sendIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, intent.getStringExtra(SunshineService.LOCATION_QUERY_EXTRA));
+            context.startService(sendIntent);
+        }
+    }
 }
